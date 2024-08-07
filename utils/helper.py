e7ae706ce2d9e7cec5ae2c16a9aedeaecd1c52d7
Karamjeet <karamjeet@d>
2024-08-07 11:29:49 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -2,4 +2,4 @@ import os
 
 MODELS = ["CompVis/stable-diffusion-v1-4", "CompVis/stable-diffusion-v-1-4-original","Linaqruf/anything-v3.0"]
 IMG_DIR="generated_images"
-BASE_DIR= os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+BASE_DIR= os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
